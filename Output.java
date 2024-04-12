7ad2c3d536fba154a4d1c9c9f5b0aec2b1f0c55e
Yoruori <tigerbazooka12345@gmail.com>
2024-04-12 15:20:07 +0900

    処理の分割

--- Output.java
+++ Output.java
@@ -4,9 +4,11 @@ import java.io.FileWriter;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.Calendar;
+import java.text.SimpleDateFormat;
 
 public class Output {
-    public static void bananasOutput(String Bananas, Scanner scanner) {
+    public static File bananasOutput(Scanner scanner) {
         JFileChooser ch = new JFileChooser();
         ch.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
         String strclass = System.getProperty("user.dir");
@@ -25,19 +27,26 @@ public class Output {
             int result = ch.showOpenDialog(null);
             if (result == JFileChooser.APPROVE_OPTION) {
                 File file = ch.getSelectedFile();
-                writeToFile(file, Bananas);
+                return file;
             } else {
                 System.out.println("ファイルの選択がキャンセルされました．");
+                return ch.getSelectedFile();
             }
         } else {
             File file = ch.getSelectedFile();
-            writeToFile(file, Bananas);
+            return file;
         }
+
     }
 
-    private static void writeToFile(File file, String content) {
+    public static void writeToFile(File file, String content) {
         System.out.println(file.toString() + "/Bananas.txtにバナナスを出力しました．");
+        System.out.println("");
+        Calendar cl = Calendar.getInstance();
+
+        SimpleDateFormat sdf = new SimpleDateFormat("[yyyy/MM/dd HH:mm:ss] ");
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(file.toString() + "/Bananas.txt", true))) {
+            bw.write(sdf.format(cl.getTime()));
             bw.write(content);
             bw.newLine();
         } catch (IOException e) {
