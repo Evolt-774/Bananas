22a981555ff70468b12f754e4999e8567f5ac954
Yoruori <tigerbazooka12345@gmail.com>
2024-04-12 15:18:52 +0900

    処理順の変更

--- Bananas.java
+++ Bananas.java
@@ -1,39 +1,42 @@
-import java.util.*;
+import java.util.Scanner;
+import java.io.File;
 
 public class Bananas{
     public static void main(String[] args) {
         Scanner std = new Scanner(System.in);
-        Concat cc = new Concat();
-
         String first, last;
-
         String result;
+        File outputPath;
 
         System.out.println("バナナのナス，バナナスを作ります");
-        System.out.println("任意のAのBを入力してください");
-        
-        do{
+        outputPath = Output.bananasOutput(std);
+
+        while(true){
+            System.out.println("任意のAのBを入力してください");
+            System.out.println("終了する場合は両方に何も入力しないでください．");
+
             System.out.println("[A]");
             first = std.nextLine();
-        }while((first == ""));
-        
-        do{
+
             System.out.println("[B]");
             last = std.nextLine();
-        }while((last == ""));
 
-
-        
-        result = cc.concatBananas(first, last);
-
-        Output.bananasOutput(result, std);
-
-
-        System.out.println("");
-
-        System.out.println(result);
-
-        System.out.println("");
-        
+            if((first == "") && (last == "")){
+                System.out.println("バナナスを終了します");
+                System.exit(0);
+            }
+    
+    
+            result = Concat.concatBananas(first, last);
+    
+    
+            System.out.println("");
+    
+            System.out.println(result);
+    
+            System.out.println("");
+    
+            Output.writeToFile(outputPath, result);
+        }
     }
 }
